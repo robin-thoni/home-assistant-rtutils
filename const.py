5960caeac10822e573bef41588f275c32dae2f99
Robin THONI <robin@rthoni.com>
2025-11-28 17:50:47 +0100

    Added load_yaml service

--- const.py
+++ const.py
@@ -6,3 +6,7 @@ DOMAIN = "rtutils"
 DEFAULT_NAME = "RT Utils"
 
 SERVICE_DUMMY = "dummy"
+
+SERVICE_LOAD_YAML = "load_yaml"
+
+ATTR_SERVICE_LOAD_YAML_DATA = "data"
--- services.py
+++ services.py
@@ -4,18 +4,32 @@ from __future__ import annotations
 
 import voluptuous as vol
 
-from homeassistant.core import HomeAssistant, ServiceCall, callback
+from homeassistant.core import (
+    HomeAssistant,
+    ServiceCall,
+    callback,
+    SupportsResponse,
+)
 from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.service import async_register_admin_service
+
+import yaml
 
 from .const import (
     _LOGGER,
     DOMAIN,
     SERVICE_DUMMY,
+    SERVICE_LOAD_YAML,
+    ATTR_SERVICE_LOAD_YAML_DATA,
 )
 
 def dummy(service: ServiceCall) -> None:
     _LOGGER.debug("Dummy service called with data: %s", service.data)
+
+
+def load_yaml(service: ServiceCall) -> dict:
+    data_dict = yaml.safe_load(service.data[ATTR_SERVICE_LOAD_YAML_DATA])
+    return data_dict
 
 
 @callback
@@ -30,4 +44,15 @@ def async_setup_services(hass: HomeAssistant) -> None:
             {
             }
         ),
+    )
+    hass.services.async_register(
+        DOMAIN,
+        SERVICE_LOAD_YAML,
+        load_yaml,
+        schema=vol.Schema(
+            {
+                vol.Required(ATTR_SERVICE_LOAD_YAML_DATA): cv.string,
+            }
+        ),
+        supports_response=SupportsResponse.ONLY,
     )
