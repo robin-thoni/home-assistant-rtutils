cfae2da81d88433dde2fb6bdeeedb005585eb480
Robin THONI <robin@rthoni.com>
2025-12-14 16:53:07 +0100

    Added `svg_to_png` action

--- const.py
+++ const.py
@@ -10,3 +10,10 @@ SERVICE_DUMMY = "dummy"
 SERVICE_LOAD_YAML = "load_yaml"
 
 ATTR_SERVICE_LOAD_YAML_DATA = "data"
+
+SERVICE_SVG_TO_PNG = "svg_to_png"
+
+ATTR_SERVICE_SVG_TO_PNG_SRC = "svg_path"
+ATTR_SERVICE_SVG_TO_PNG_DST = "png_path"
+ATTR_SERVICE_SVG_TO_PNG_WIDTH = "png_width"
+ATTR_SERVICE_SVG_TO_PNG_HEIGHT = "png_height"
--- services.py
+++ services.py
@@ -10,6 +10,7 @@ from homeassistant.core import (
     callback,
     SupportsResponse,
 )
+from homeassistant.exceptions import HomeAssistantError
 from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.service import async_register_admin_service
 
@@ -21,6 +22,11 @@ from .const import (
     SERVICE_DUMMY,
     SERVICE_LOAD_YAML,
     ATTR_SERVICE_LOAD_YAML_DATA,
+    SERVICE_SVG_TO_PNG,
+    ATTR_SERVICE_SVG_TO_PNG_SRC,
+    ATTR_SERVICE_SVG_TO_PNG_DST,
+    ATTR_SERVICE_SVG_TO_PNG_WIDTH,
+    ATTR_SERVICE_SVG_TO_PNG_HEIGHT,
 )
 
 def dummy(service: ServiceCall) -> None:
@@ -30,6 +36,22 @@ def dummy(service: ServiceCall) -> None:
 def load_yaml(service: ServiceCall) -> dict:
     data_dict = yaml.safe_load(service.data[ATTR_SERVICE_LOAD_YAML_DATA])
     return data_dict
+
+
+def svg_to_png(service: ServiceCall) -> None:
+
+    try:
+        from cairosvg import svg2png
+    except:
+        raise HomeAssistantError("CairoSVG is not installed")
+    with open(service.data[ATTR_SERVICE_SVG_TO_PNG_SRC], 'rt') as svg_file:
+        svg_code = svg_file.read()
+    svg2png(
+        bytestring=svg_code,
+        write_to=service.data[ATTR_SERVICE_SVG_TO_PNG_DST],
+        output_width=service.data.get(ATTR_SERVICE_SVG_TO_PNG_WIDTH),
+        output_height=service.data.get(ATTR_SERVICE_SVG_TO_PNG_HEIGHT),
+    )
 
 
 @callback
@@ -55,4 +77,17 @@ def async_setup_services(hass: HomeAssistant) -> None:
             }
         ),
         supports_response=SupportsResponse.ONLY,
+    )
+    hass.services.async_register(
+        DOMAIN,
+        SERVICE_SVG_TO_PNG,
+        svg_to_png,
+        schema=vol.Schema(
+            {
+                vol.Required(ATTR_SERVICE_SVG_TO_PNG_SRC): cv.string,
+                vol.Required(ATTR_SERVICE_SVG_TO_PNG_DST): cv.string,
+                vol.Optional(ATTR_SERVICE_SVG_TO_PNG_WIDTH): cv.positive_int,
+                vol.Optional(ATTR_SERVICE_SVG_TO_PNG_HEIGHT): cv.positive_int,
+            }
+        ),
     )
